2bd487666ec18ed65653b2c6dd2cb0b62a07a5e7
connorwfitch <connorwfitch@gmail.com>
2022-02-17 10:56:02 -0500

    building up keyboard functionality

--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
   createSquares();
-  
+  const keys = document.querySelectorAll('.keyboard-row button');
+
+  for (let i = 0; i < keys.length; i++) {
+    keys[i].onclick = ({target}) => {
+      const key = target.getAttribute('data-key');
+    }
+    
+  }
+
   function createSquares() {
     const gameBoard = document.getElementById('board');
-    for (let index = 0; index < 30; index++) {
+    for (let i = 0; i < 30; i++) {
       let square = document.createElement('div');
       square.classList.add('square');
-      square.setAttribute('id', index + 1);
+      square.setAttribute('id', i + 1);
       gameBoard.appendChild(square);
     }
   }
-})
+
+
+});
