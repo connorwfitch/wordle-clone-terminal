4c6217b00728f4dfbed8bc0deade3d7195d1f813
connorwfitch <connorwfitch@gmail.com>
2022-02-18 13:24:15 -0500

    added js checking function along with corresponding  color change in css

--- js/main.js
+++ js/main.js
@@ -7,7 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // initializing my guessed words and my tracker for the current box id
   let guessedWords = [[]];
-  let availableSpace = 1;
+  let availableSpace = 0;
+
+  // defining my secret word as an array
+  const secretWord = 'frank';
 
   // defining the basic keyboard inputs
   for (let i = 0; i < keys.length; i++) {
@@ -47,19 +50,59 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     } else if (letter === 'enter' && currentWordArray.length === 5) {
       // enter handling
-      // need to add logic for checking the word
-      // start a new line
+      checkWord(currentWordArray);
     }
   }
 
-  // creates my games squares, with id's that are 1 indexed
+  // creates my games squares, with id's that are 0 indexed
   function createSquares() {
     const gameBoard = document.getElementById('board');
     for (let i = 0; i < 30; i++) {
       let square = document.createElement('div');
       square.classList.add('square');
-      square.setAttribute('id', i + 1);
+      square.setAttribute('id', i);
       gameBoard.appendChild(square);
+    }
+  }
+
+  function checkWord(wordArray) {
+    let check = secretWord.split('');
+    let greens = [false,false,false,false,false];
+    let yellows = [false,false,false,false,false];
+    // green check
+    for (let i = 0; i < 5; i++) {
+      console.log(wordArray[i],secretWord[i],wordArray[i] === secretWord[i]);
+      if (wordArray[i] === secretWord[i]) {
+        // remove the letter from check
+        const index = check.indexOf(wordArray[i]);
+        check.splice(index,1);
+
+        // set greens
+        greens[i] = true;
+      }
+    }
+    // yellow check
+    for (let i = 0; i < 5; i++) {
+      if (check.includes(wordArray[i]) && !greens[i]) {
+        // remove the letter from check
+        const index = check.indexOf(wordArray[i]);
+        check.splice(index, 1);
+
+        // set yellows
+        yellows[i] = true;
+      }
+    }
+
+    // add colors
+    let currentSpace = availableSpace - 5;
+    for (let i = 0; i < 5; i++) {
+      const workingEle = document.getElementById(String(currentSpace));
+      if(greens[i]) {
+        workingEle.classList.add('green');
+      } else if(yellows[i]) {
+        workingEle.classList.add('yellow');
+      }
+      currentSpace ++;
     }
   }
 
