dc2f168ed2045dfefdf31b34b0fd820f52bf886c
connorwfitch <connorwfitch@gmail.com>
2022-02-18 11:15:24 -0500

    greater keyboard functionality and display into boxes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
   createSquares();
+
   const keys = document.querySelectorAll('.keyboard-row button');
+  let guessedWords = [[]];
+  let availableSpace = 1;
 
   for (let i = 0; i < keys.length; i++) {
     keys[i].onclick = ({target}) => {
-      const key = target.getAttribute('data-key');
+      const letter = target.getAttribute('data-key');
+      updateGuessedWords(letter);
     }
     
+  }
+
+  function getCurrentWordArray() {
+    const numberGuessedWords = guessedWords.length;
+    return guessedWords[numberGuessedWords-1];
+  }
+
+  function updateGuessedWords(letter) {
+    const currentWordArray = getCurrentWordArray();
+
+    if(currentWordArray && currentWordArray.length < 5 ||
+       'enterdel'.includes(letter) && availableSpace>1) {
+      if(letter === 'del') {
+        currentWordArray.pop();
+        availableSpace -= 1;
+        const availableSpaceEl = document.getElementById(String(availableSpace));
+        availableSpaceEl.textContent = '';
+      } else {
+        currentWordArray.push(letter);
+
+        const availableSpaceEl = document.getElementById(String(availableSpace));
+        availableSpace += 1;
+
+        availableSpaceEl.textContent = letter;
+      }
+    }
   }
 
   function createSquares() {
